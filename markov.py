5163cd3422e86eafde56bbe22152fc4c224d02ab
miedzinski <dominik@mdzn.pl>
2015-07-14 22:23:09 +0200

    rewritten to store words in redis

--- markov.py
+++ markov.py
@@ -1,94 +1,88 @@
 #!/usr/bin/env python3
 
+import configparser
 import irc.bot
-import pickle
 import random
+import redis
+import sys
 import time
-
-DB_PATH = 'db'
-HOST = 'irc.freenode.net'
-PORT = 6667
-NICK = ''
-REALNAME = ''
-CHANNEL = '#'
-CHATTINES = 0.05  # chance of bot talking, 0 - 1.00
-DELAY = 299  # frequency of how often bot has a chance to randomly speak
-BLACKLIST = []  # nicknames to ignore
 
 
 class Markov:
 
-    def __init__(self):
-        with open(DB_PATH, 'rb') as f:
-            self.cache = pickle.load(f)
+    def __init__(self, config):
+        self.client = redis.StrictRedis(host=config['host'],
+                                        port=config['port'],
+                                        db=config['db'],
+                                        password=config['password'])
 
     def add_words(self, words):
         words.append('\n')
         for i, word in enumerate(words):
             try:
-                first, second, third = words[i], words[i + 1], words[i + 2]
+                key, completion = (words[i], words[i + 1]), words[i + 2]
             except IndexError:
                 break
-            key = (first, second)
-            if key not in self.cache:
-                self.cache[key] = []
-            self.cache[key].append(third)
-
-    def dump_db(self):
-        with open(DB_PATH, 'wb') as f:
-            pickle.dump(self.cache, f)
+            key = ' '.join(key)
+            self.client.zincrby(key, completion)
 
     def generate_sentence(self):
-        key = random.choice([key for key in self.cache.keys()
-                             if '\n' not in key])
+        key = self.client.randomkey().decode()
+        sentence = [key]
 
-        sentence = list()
-        first, second = key
-        sentence.extend(key)
-
-        while key in self.cache:
-            third = random.choice(self.cache[key])
-            key = (second, third)
-            second = third
-            if third != '\n':
-                sentence.append(third)
+        while self.client.exists(key):
+            zset = self.client.zrevrange(key, 0, -1, withscores=True)
+            completions = [[c[0].decode()] * int(c[1]) for c in zset]
+            # let's flatten our list
+            completions = [item for sublist in completions for item in sublist]
+            completion = random.choice(completions)
+            if completion != '\n':
+                sentence.append(completion)
+            key = '{} {}'.format(key.split()[1], completion)
 
         return ' '.join(sentence)
 
 
 class MarkovBot(irc.bot.SingleServerIRCBot):
 
-    def __init__(self, markov):
+    def __init__(self, markov, config):
         irc.client.ServerConnection.buffer_class.errors = 'replace'
         irc.bot.SingleServerIRCBot.__init__(self,
-                                            [(HOST, PORT)],
-                                            NICK,
-                                            REALNAME)
+                                            [(config['host'],
+                                              int(config['port']))],
+                                            config['nick'],
+                                            config['realname'])
         self.markov = markov
-        self.connection.execute_every(DELAY, self.say)
+        self.channel = config['channel']
+        self.chattines = float(config['chattines'])
+        self.blacklist = config['blacklist'].split(',')
 
     def on_welcome(self, c, e):
-        c.join(CHANNEL)
+        c.join(self.channel)
 
     def on_kick(self, c, e):
         time.sleep(3)
         c.join(e.target)
 
     def on_pubmsg(self, c, e):
-        if e.source.nick not in BLACKLIST:
+        if e.source.nick not in self.blacklist:
             self.markov.add_words(e.arguments[0].split())
-            self.markov.dump_db()
         if c.nickname in e.arguments[0]:
             c.privmsg(e.target, self.markov.generate_sentence())
 
     def say(self):
-        if self.connection.connected and random.random() <= CHATTINES:
-            self.connection.privmsg(CHANNEL, self.markov.generate_sentence())
+        connection = self.connection
+        if connection.connected and random.random() <= self.chattines:
+            connection.privmsg(self.channel, self.markov.generate_sentence())
 
 
 def main():
-    mc = Markov()
-    bot = MarkovBot(mc)
+    if len(sys.argv) != 2:
+        sys.exit('usage: python3 {} <config-file>'.format(sys.argv[0]))
+    config = configparser.ConfigParser()
+    config.read(sys.argv[1])
+    mc = Markov(config['redis'])
+    bot = MarkovBot(mc, config['irc'])
     bot.start()
 
 if __name__ == '__main__':
